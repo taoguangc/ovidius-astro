e339f461a139e4e7b4aa080e0378db9806f61781
taoguangc <taoguangc@gmail.com>
2025-06-14 14:59:33 +0800

    删除了不再使用的 astro.config.mjs 文件，更新了 package.json 中的依赖版本，添加了新的依赖项，包括 astro-icon 和 tinacms。同时，pnpm-lock.yaml 文件也进行了相应的更新，以反映这些更改。

---
+++ astro.config.ts
@@ -0,0 +1,23 @@
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
+
+import icon from 'astro-icon';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://ovidius-astro.vercel.app',
+    vite: {
+        plugins: [tailwindcss()]
+    },
+    integrations: [mdx(), sitemap(), icon({
+        iconDir: 'src/assets/icons'
+    })],
+    image: {
+        experimentalLayout: 'constrained'
+    },
+    experimental: {
+        responsiveImages: true
+    },
+});
